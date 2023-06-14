c2d448f8d612226c68f173e66ae3b5dc5da31264
Peter Hirt <peter.hirt@orange.fr>
2023-06-14 08:15:16 +0200

    initial

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+   <h1>Hi Peter</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const Authorization =  `Bearer ${config.OPENAI_API_KEY}`
+console.log(Authorization)
+</script>

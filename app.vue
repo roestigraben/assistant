bc94cd33bc1a8847fbd6886675a21491f93c52e5
Peter Hirt <peter.hirt@orange.fr>
2023-06-14 12:31:59 +0200

    standard clone version OK

--- app.vue
+++ app.vue
@@ -1,11 +1,157 @@
 <template>
-  <div>
-   <h1>Hi Peter</h1>
+  <div class="light">
+    <main
+      class="absolute inset-0 transition-width flex flex-col overflow-hidden items-stretch flex-1 light:bg-gray-800 pb-20 md:pb-32">
+      <div class="flex-1 overflow-hidden">
+        <div class="h-full overflow-y-auto">
+          <div class="flex flex-col items-center text-sm h-full chat-messages">
+            <div v-for="message in messages"
+              class="w-full border-b border-black/10 light:border-gray-900/50 text-gray-800 light:text-gray-100 group"
+              :class="{ 'bg-gray-100': message.actor === 'Human', 'light:bg-gray-700': message.actor === 'AI' }">
+              <div
+                class="text-base gap-4 md:gap-6 m-auto md:max-w-2xl lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0">
+                <div class="w-[30px] flex flex-col relative items-end">
+                  <div v-if="message.actor === 'Human'" class="relative flex">
+                    <Icon name="uil:user" height="large" size="large" class="bg-indigo-300" />
+                  </div>
+
+                  <div v-else
+                    class="relative h-[30px] w-[30px] p-1 rounded-sm text-white flex items-center justify-center bg-emerald-700">
+                  </div>
+                </div>
+                <div class="relative flex w-[calc(100%-50px)] md:flex-col lg:w-[calc(100%-115px)]">
+                  <div class="flex flex-grow flex-col gap-3">
+                    <div
+                      class="min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap prose prose-gray dark:prose-invert prose-p:m-0 prose-pre:p-0 prose-pre:m-0 prose-li:my-0 prose-li:leading-none prose-ol:my-0">
+                      <VueShowdown :markdown="addFullBlock(message.message, message.loading)"
+                        :extensions="['highlight']" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+
+    <div
+      class="fixed inset-x-0 bottom-0 left-0 border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient">
+
+
+      <form class="stretch mx-2 flex flex-row gap-3 pt-2 last:mb-2 md:last:mb-6 lg:mx-auto lg:max-w-3xl lg:pt-6">
+        <div class="relative flex h-full flex-1 md:flex-col">
+          <div class="ml-1 mt-1.5 md:w-full md:m-auto md:flex md:mb-2 gap-2 justify-center"></div>
+          <div
+            class="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
+            <textarea tabindex="0" v-model="message" @keydown.enter.exact.prevent="submit()" ref="messageInput"
+              autofocus :disabled="loading" data-id="63ee3844-11f0-456d-b3b5-a6e2a3ac6a04" rows="1"
+              :class="{ 'text-gray-800': loading }" placeholder="Write your message here..."
+              class="m-0 w-full resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0"
+              style="max-height: 200px; overflow-y: scroll;" :style="{ height: (numOfLines * 24) + 'px' }"></textarea>
+            <button @click.prevent="submit()" :disabled="loading"
+              class="absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1 md:bottom-2.5 md:right-2 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent"></button>
+          </div>
+        </div>
+      </form>
+
+
+
+      <div class="px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6"><a
+          href="https://beta.openai.com" target="_blank" rel="noreferrer" class="underline">based on OpenAI models augmented by enterprise data,
+          </a></div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const config = useRuntimeConfig();
-const Authorization =  `Bearer ${config.OPENAI_API_KEY}`
-console.log(Authorization)
+// import { ref } from 'vue'
+// const config = useRuntimeConfig();
+// const Authorization =  `Bearer ${config.OPENAI_API_KEY}`
+
+
+const messages = ref([{
+  actor: 'AI',
+  message: 'Hello! How can I help you?',
+  loading: false
+},{
+  actor: 'Human',
+  message: 'are you a bot?',
+  loading: false
+},{
+  actor: 'AI',
+  message: 'I am an assistant to your personal data',
+  loading: false
+}, {
+  actor: 'Human',
+  message: 'thank you',
+  loading: false
+}]);
+
+const message = ref<string>("")
+const loading = ref(false);
+const messageInput = ref<HTMLInputElement | null>(null)
+
+const addMessage = (actor: "AI" | "Human", message: string, loading: boolean = true) => {
+  const length = messages.value.push({ actor, message, loading });
+  scrollToEnd();
+  return messages.value[length - 1];
+};
+
+const addFullBlock = (markdown: string, loading: boolean) => {
+  return markdown + (loading ? '<span class="animate-pulse">\u258b</span>' : '')
+};
+
+const numOfLines = computed(() => {
+  return Math.max(Math.min(4, message.value.split('\n').length), 1);
+});
+
+const scrollToEnd = () => {
+  setTimeout(() => {
+    document.querySelector(".chat-messages>div:last-child")?.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  }, 100);
+};
+
+const sendRequest = async () => {
+  loading.value = true;
+  const newMessage = addMessage("AI", '');
+
+  const res = await fetch(`/api/gpt3`, {
+    body: JSON.stringify(messages.value.slice(1)),
+    method: 'post'
+  });
+
+  const reader = res.body?.pipeThrough(new TextDecoderStream()).getReader();
+
+  while (true) {
+    const result = await reader?.read();
+    console.log('Result', result);
+
+    if (result?.done) {
+      loading.value = false;
+      newMessage.loading = false;
+      return;
+    }
+
+    const results = result?.value?.split(/\n/);
+    results?.forEach((line) => {
+      const dataSlice = line.slice('data: '.length).trim();
+      if (dataSlice && dataSlice != '[DONE]') {
+        const data = JSON.parse(dataSlice);
+        newMessage.message = newMessage.message.concat(data.text);
+        scrollToEnd();
+      }
+    });
+  }
+}
+
+const submit = async () => {
+  const newMessage = message.value;
+  addMessage("Human", newMessage, false);
+  await sendRequest();
+  message.value = "";
+  messageInput.value?.focus();
+};
+
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
   runtimeConfig: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    public: {
+      apiBase: '/api'
+    }
   },
 })

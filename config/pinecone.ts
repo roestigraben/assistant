97bc89d9640846e7ccada07f9d6c0e896882f638
Peter Hirt <peter.hirt@orange.fr>
2023-07-11 10:38:19 +0200

    small change on namespace

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -3,12 +3,11 @@
  */
 
 if (!process.env.PINECONE_INDEX_NAME) {
-    throw new Error('Missing Pinecone index name in .env file');
-  }
-  
-  const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
-  
-  const PINECONE_NAME_SPACE = 'demo1'; //namespace is optional for your vectors
-  
-  export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
-  
+  throw new Error('Missing Pinecone index name in .env file');
+}
+
+const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
+
+const PINECONE_NAME_SPACE = 'bytefulJulx'; //namespace is optional for your vectors
+
+export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
